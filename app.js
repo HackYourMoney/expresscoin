f2166314bc864606e413b577eff696f5490d8fd7
854575-PC\854575 <854575@naver.com>
2018-01-08 16:50:35 +0900

    init error 해결

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var connect = require('connect-mongo');
+var passport = require('passport');
+var favicon = require('serve-favicon');
 
 var index = require('./router');
-var users = require('./server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './server/views'));
 app.set('view engine', 'ejs');
 
 
 
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.jpg')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.jpg')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,7 +44,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{ title:'Error' });
 });
 
 module.exports = app;
